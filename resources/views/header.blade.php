1f18f3218feace11fd9723373d97d2652c5ffe4d
francesco.miceli <francescomiceli@outlook.com>
2022-11-01 16:03:41 +0100

    nav in progress

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,8 +1,29 @@
 <div id="strip_one">
-    ciao
+    <div class="container">
+        <p class="">DC POWER VISA</p>
+        <p class="">ADDITIONAL DC SITES</p>
+    </div>
 </div>
-<div id="logo">
-    <img src="{{asset('images/dc-logo.png')}}" id="logo"  alt="logo">
+<div class="container_nav">
+    <div id="logo">
+        <img src="{{asset('images/dc-logo.png')}}" id="logo"  alt="logo">
+    </div>
+
+    <div class="navigation">
+        <ul>
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAME</li>
+            <li>VIDEO</li>
+            <li>FANS</li>
+            <li>MOVIES</li>
+            <li>SHOP</li>
+            <li>SHOP</li>
+        </ul>
+    </div>
 </div>
+
 
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.2/js/bootstrap.min.js" integrity="sha512-5BqtYqlWfJemW5+v+TZUs22uigI8tXeVah5S/1Z6qBLVO7gakAOtkOzUtgq6dsIo5c0NJdmGPs0H9I+2OHUHVQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <title>Home Page</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"> --}}
+    <title>Dc-Comics</title>
 </head>
 <body>
 
 @include('header')
-<h1>Hello world</h1>
+
 
 
 </body>
