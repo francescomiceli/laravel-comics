7a0dbd176f5fc202d8708705d118b4bef50459c5
francesco.miceli <francescomiceli@outlook.com>
2022-11-01 16:24:36 +0100

    NAvigazione completa

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,29 +1,40 @@
+ {{-- BARRA SUPERIORE --}}
+
 <div id="strip_one">
     <div class="container">
         <p class="">DC POWER VISA</p>
         <p class="">ADDITIONAL DC SITES</p>
     </div>
 </div>
+
+{{-- NAVIGAZIONE --}}
+
 <div class="container_nav">
     <div id="logo">
-        <img src="{{asset('images/dc-logo.png')}}" id="logo"  alt="logo">
+        <a href="#"><img src="{{asset('images/dc-logo.png')}}" id="logo"  alt="logo"></a>
     </div>
 
     <div class="navigation">
         <ul>
-            <li>CHARACTERS</li>
-            <li>COMICS</li>
-            <li>MOVIES</li>
-            <li>TV</li>
-            <li>GAME</li>
-            <li>VIDEO</li>
-            <li>FANS</li>
-            <li>MOVIES</li>
-            <li>SHOP</li>
-            <li>SHOP</li>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAME</a></li>
+            <li><a href="#">VIDEO</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">SHOP</a></li>
         </ul>
     </div>
+
+    <div class="search">
+        <p><a href="">Search</a></p>
+        <i class="fa-solid fa-magnifying-glass"></i>
+    </div>
 </div>
+
+{{-- FINE NAVIGAZIONE --}}
 
 
 
